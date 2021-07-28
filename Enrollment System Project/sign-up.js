3933a7dae94dd000ca5c0a299f34798a6a62166f
Adrian Nads <marcelonads@gmail.com>
2021-07-28 18:24:02 +0800

    fixed bugs

--- Enrollment System Project/sign-up.js
+++ Enrollment System Project/sign-up.js
@@ -2,12 +2,14 @@
 
 import Student  from "./Modules/student.js";
 
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Event for the dropdown menu for countries.
     document.querySelector('#countries').addEventListener('change', function() {
 
         let otherCountryField = document.querySelector('.other-countries');
+        let otherCountry = document.querySelector('#other-country');
 
         if (this.value === 'Other') {
 
@@ -17,15 +19,21 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 300);
             otherCountryField.style.animation = 'showAnimation 300ms';
             otherCountryField.style.display = 'block';
+            otherCountryField.classList.add('input-field');
+            otherCountry.classList.add('input-field');
         } 
-        else
+        else {
             otherCountryField.style.display = 'none';
+            otherCountry.classList.remove('input-field');
+        }
+            
     })
 
     // Event for the dropdown menu for courses.
     document.querySelector('#course').addEventListener('change', function() {
 
         let otherCourseField = document.querySelector('.other-course');
+        let otherCourse = document.querySelector('#other-course');
 
         if (this.value === 'Other') {
 
@@ -35,14 +43,41 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 300);
             otherCourseField.style.animation = 'showAnimation 300ms';
             otherCourseField.style.display = 'block';
+            otherCourse.classList.add('input-field');
         }
-        else
+        else {
             otherCourseField.style.display = 'none';
+            otherCourse.classList.remove('input-field');
+        }
+            
     });
+
+    // Event for button.
+
+    document.querySelector('button').addEventListener('click', () => {
+
+        if (!isAllFilledUp())
+            alert('Please fill up');
+        else
+            alert('success');
+    })
 });
 
 function isAllFilledUp() {
 
+    let inputFields = document.querySelectorAll('.input-field');
+    let isFilledUp = true;
 
-    
+    inputFields.forEach(inputField => {
+
+        if (!(String(inputField.value))) {
+
+            isFilledUp = false;
+            inputField.style.border = '2px solid red';
+            
+            inputField.addEventListener('change', () => inputField.style.border = 'none');
+        }
+    });
+
+    return isFilledUp;
 }
