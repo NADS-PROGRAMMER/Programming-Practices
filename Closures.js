1c8ec1f621bcc498f9fc15da80e54e8718c452a6
Adrian Nads <marcelonads@gmail.com>
2021-07-29 10:09:56 +0800

    done for input checking in my sign up page, but need to check for the valid email

--- Closures.js
+++ Closures.js
@@ -1,12 +1,21 @@
 "use strict"
 
-const greetings = (greet) => {
+function myFunc(callback) {
 
-    return (yourName) => {
-
-        console.log(greet + " " + yourName);
-    }
+    callback();
 }
 
-const myName = greetings('Hello');
-myName('Nads');
+function Callback(name, callback) {
+
+    setTimeout(() => {
+
+        callback('nads', console.log('Callback', name));
+    }, 2000)
+    
+}
+
+function Callback2(name) {
+
+    console.log('Callback2', name);
+}
+myFunc(() => Callback('Im Nads', Callback2));
--- Enrollment System Project/sign-up.js
+++ Enrollment System Project/sign-up.js
@@ -2,5 +2,35 @@
 
 import Student  from "./Modules/student.js";
 
+function addInputEvents() {
 
+    let inputFields = document.querySelectorAll('.input-field');
 
+    for (let index = 0; index < inputFields.length; index++) {
+
+        inputFields[index].addEventListener('input', () => {
+
+            if (!(inputFields[index].value)) {
+
+                inputFields[index].style.border = '1px solid #d21c38';
+                inputFields[index].nextElementSibling.style.display = 'block';
+            }
+            else {
+
+                inputFields[index].style.border = 'none';
+                inputFields[index].style.borderBottom = '2px solid #657786';
+                inputFields[index].nextElementSibling.style.display = 'none';
+            } 
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    addInputEvents();
+
+    document.querySelector('button').addEventListener('submit', () => {
+
+        return true;
+    })
+});
