6038e835250f7e4a268ef336e30155c3bfba2b88
Adrian Nads <marcelonads@gmail.com>
2021-07-31 15:10:33 +0800

    fixed the bug in email js

--- Enrollment System Project/Modules/email.js
+++ Enrollment System Project/Modules/email.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
             button.disabled = true;
     })
 
-    button.addEventListener('submit', () => {
+    button.addEventListener('click', () => {
 
         let newStudent = JSON.parse(sessionStorage.getItem('newStudent'));
         let enrolledStudent = [];
@@ -26,16 +26,39 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.querySelector('#password');
         const confirmedPassword = document.querySelector('#confirm-pass');
 
+        // Check if email does not exist.
+        if (isEmailExist(email.value)) {
+
+            setErrorStyles(email, 'Email does exist.');
+            return;
+        }
+
+        // Checks if the password and the confirmed password is matched/correct.
         if (isPasswordMatch(password.value, confirmedPassword.value)) {
 
             newStudent['studentEmail'] = email.value;
             newStudent['studentPassword'] = password.value;
-        } else {
+        } 
+        else {
 
-            
+            setErrorStyles(confirmedPassword, 'Confirmed password does not match. Kindly check it.');
+            return;
         }
+            
         
+        // Event in confirmed password.
+        confirmedPassword.addEventListener('input', () => {
 
+            onInputStyles(confirmedPassword);
+        })
+
+        // Event of email input.
+        email.addEventListener('input', () => {
+
+            onInputStyles(email);
+        })
+
+        // Storing the new student.
         if (localStorage.getItem('enrolledStudent') == null || localStorage.getItem('enrolledStudent') == undefined) {
 
             enrolledStudent = [newStudent];
@@ -52,6 +75,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function isPasswordMatch(password, confirmPassword) {
 
-    return password === confirmPassword;
+    return String(password) === String(confirmPassword);
+}
+
+function isEmailExist(thisEmail) {
+
+    let enrolledStudent = JSON.parse(localStorage.getItem('enrolledStudent'));
+
+    for (let student of enrolledStudent) {
+
+        if (student['studentEmail'] === String(thisEmail)) 
+            return true;
+    }
+    return false;
+}
+
+function setErrorStyles(element, errorMessage) {
+
+    element.style.animation = 'errorMessageAnimation 300ms';
+    element.style.border = 'none';
+    element.style.border = '1px solid #d21c38';
+    element.nextElementSibling.textContent = errorMessage;
+    element.nextElementSibling.style.animation = 'errorMessage 300ms';
+    element.nextElementSibling.style.display = 'block';
+}
+
+function onInputStyles(element) {
+
+    element.style.animation = 'errorMessageAnimation 300ms';
+    element.style.border = 'none';
+    element.style.borderBottom = '2px solid #657786';
+    element.nextElementSibling.style.animation = 'errorMessageDisappearing 300ms';
+    element.nextElementSibling.style.display = 'none';
 }
 console.log(JSON.parse(localStorage.getItem('enrolledStudent')));
