668fe8bce26d2175b30bb9b34d5716e51e2ad003
Adrian Nads <marcelonads@gmail.com>
2021-08-01 08:51:30 +0800

    created tribute page files

---
+++ Javascript Files/PalindromeNumber.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const isPalindrome = (num) => {
+
+    if (num < 0)
+        return false;
+
+    let reversedNumber = 0;
+
+    while (num > 0) {
+
+        reversedNumber = (num % 10) 
+    }
+}
